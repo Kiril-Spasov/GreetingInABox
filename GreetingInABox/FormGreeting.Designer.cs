94e0962b7dc1afe49209580a651719ea3f0f3dc6
Kiril Spasov <spasov.kiril@gmail.com>
2022-12-08 10:50:33 +0200

    Add project files.

---
+++ GreetingInABox/FormGreeting.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace GreetingInABox
+{
+    partial class FormGreeting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnDisplay = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnDisplay
+            // 
+            this.BtnDisplay.Location = new System.Drawing.Point(40, 53);
+            this.BtnDisplay.Name = "BtnDisplay";
+            this.BtnDisplay.Size = new System.Drawing.Size(197, 68);
+            this.BtnDisplay.TabIndex = 0;
+            this.BtnDisplay.Text = "Display Greeting";
+            this.BtnDisplay.UseVisualStyleBackColor = true;
+            this.BtnDisplay.Click += new System.EventHandler(this.BtnDisplay_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TxtResult.Location = new System.Drawing.Point(277, 53);
+            this.TxtResult.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(775, 432);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormGreeting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1120, 545);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnDisplay);
+            this.Name = "FormGreeting";
+            this.Text = "Greeting In A Box";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnDisplay;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ GreetingInABox/FormGreeting.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace GreetingInABox
+{
+    public partial class FormGreeting : Form
+    {
+        public FormGreeting()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDisplay_Click(object sender, EventArgs e)
+        {
+            string path = Application.StartupPath + @"\greeting.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            int leftMargin = Convert.ToInt32(streamReader.ReadLine());
+            int blankSpaces = Convert.ToInt32(streamReader.ReadLine());
+            string character = streamReader.ReadLine();
+            string name = streamReader.ReadLine();
+
+            TxtResult.Text += DisplayGreeting(leftMargin, blankSpaces, character, name);
+
+        }
+
+        private string DisplayGreeting(int leftMargin, int blankSpaces, string character, string name)
+        {
+            string spaces = "                                        ";
+            string result = "";
+            string greeting = "Hello there, " + name + "!";
+            int boxHeight = 1 + blankSpaces + 1 + blankSpaces + 1;
+
+            result += "123456789012345678901234567890123456789012345678901234567890123456" + Environment.NewLine;
+
+            //We itterate for each roll.
+            for (int i = 1; i <= boxHeight; i++)
+            {
+                //If it's the first or the last row, display row of characters.
+                if (i == 1 || i == boxHeight)
+                {
+                    result += spaces.Substring(0, leftMargin);
+                    for (int a = 1; a <= greeting.Length + blankSpaces * 2 + 2; a++)
+                    {
+                        result += character;
+                    }
+                    result += Environment.NewLine;
+                }
+                //If it's between the first row and the row with the greeting,
+                //and if it's between the row with the greeting and the last row,
+                //we display the below.
+                else if (i > 1 && i <= blankSpaces + 1 || i > boxHeight / 2 + 1 && i < boxHeight)
+                {
+                    result += spaces.Substring(0, leftMargin);
+                    result += character;
+                    result += spaces.Substring(0, greeting.Length + blankSpaces * 2);
+                    result += character;
+                    result += Environment.NewLine;
+                }
+                //Display the row with the greeting.
+                else
+                {
+                    result += spaces.Substring(0, leftMargin);
+                    result += character;
+                    result += spaces.Substring(0, blankSpaces);
+                    result += greeting;
+                    result += spaces.Substring(0, blankSpaces);
+                    result += character;
+                    result += Environment.NewLine;
+                }
+
+                result += Environment.NewLine;
+            }
+
+            return result;
+        }
+    }
+}
